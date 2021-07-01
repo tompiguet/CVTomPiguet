1ebc4b941482726a7f066121f87195bf2f859744
Tom PIGUET <tompiguet13@gmail.com>
2021-07-01 12:26:51 +0200

    petits correctifs

---
+++ assets/js/Javascript.js
@@ -0,0 +1,23 @@
+
+
+
+//Disparition du titre au scroll
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 50) {
+        document.querySelector("#navbar").style.backgroundColor = "";
+
+    } else {
+        document.querySelector("#navbar").style.backgroundColor = "";
+
+    }
+
+
+
+}
+
+
+
+
